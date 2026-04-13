d41547367883c1e46f80c090bd030d0283ea1678
NeerajBIG <neeraj.kumar@bitsinglass.com>
2026-04-13 12:48:05 -0400

    changes for importing ProjectBootstrapper files

--- BIG_QA_Solution/ScriptGenerator/app.py
+++ BIG_QA_Solution/ScriptGenerator/app.py
@@ -1,5 +1,13 @@
 import os
 import sys
+
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'ProjectBootstrapper'))
+print("Resolved path:", path)
+print("Path exists:", os.path.exists(path))
+print("Files in folder:", os.listdir(path) if os.path.exists(path) else "FOLDER NOT FOUND")
+
+sys.path.append(path)
+
 import subprocess
 import threading
 import webbrowser
@@ -11,8 +19,10 @@ import pytz
 from db.app_db import fetch_data, insert_data, update_data
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'ProjectBootstrapper')))
-from bootstrapper_engine import BootstrapperEngine
-from environment_setup import EnvironmentSetup
+from ProjectBootstrapper.bootstrapper_engine import BootstrapperEngine
+from ProjectBootstrapper.environment_setup import EnvironmentSetup
+# from bootstrapper_engine import BootstrapperEngine
+# from environment_setup import EnvironmentSetup
 
 # Global dictionary for background bootstrapper jobs
 bootstrapper_jobs = {}
